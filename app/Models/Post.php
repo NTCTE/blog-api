cb4d1f7ef953e6a17b6fd477264319c0a05d6f17
George Repiev <repev.egor@nttek.ru>
2025-11-11 17:27:00 +0500

    feat(eloquent): созданы связи в моделях и описаны модели

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'heading',
+        'body',
+        'author_id',
+        'is_draft',
+    ];
+
+    protected function casts(): array {
+        return [
+            'is_draft' => 'boolean',
+        ];
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id', 'id');
+    }
+
+    public function scopePublished(Builder $query): void
+    {
+        $query->where('is_draft', true);
+    }
+
+    public function scopeDraft(Builder $query): void
+    {
+        $query->where('is_draft', false);
+    }
 }
