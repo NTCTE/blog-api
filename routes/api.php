221f556e01b5c4c97d87f65b86895cfd60015f52
George Repiev <repev.egor@nttek.ru>
2025-11-21 12:40:43 +0500

    feat(routing): производить биндинг контроллера с маршрутом

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
 
+use App\Http\Controllers\User\RegistrationController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/user/register', [RegistrationController::class, 'register'])
+    ->middleware('guest');
