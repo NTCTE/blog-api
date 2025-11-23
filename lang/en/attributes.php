330f9ea68b526c9393be1a201d24f27ab3511c5f
George Repiev <egor.repiev@gmail.com>
2025-11-23 14:48:40 +0500

    feat(core): внедрить файлы локализации

---
+++ lang/en/attributes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'user' => [
+        'name' => '&laquo;Name&raquo;',
+        'email' => '&laquo;Email address&raquo;',
+        'password' => '&laquo;Password&raquo;',
+        'bio' => '&laquo;About&raquo;',
+    ],
+];
---
+++ lang/ru/attributes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'user' => [
+        'name' => '&laquo;Имя&raquo;',
+        'email' => '&laquo;Электронная почта&raquo;',
+        'password' => '&laquo;Пароль&raquo;',
+        'bio' => '&laquo;О себе&raquo;',
+    ],
+];
