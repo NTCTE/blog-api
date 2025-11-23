80f41dde40f5971b8b260cf7febe5d393bb16f58
George Repiev <egor.repiev@gmail.com>
2025-11-23 23:14:40 +0500

    refactor(user): обновить имя DTO для авторизации

--- app/Structures/Users/LoginDTO.php
+++ app/Structures/Users/LoginDTO.php
@@ -4,14 +4,11 @@ namespace App\Structures\Users;
 
 use Spatie\LaravelData\Data;
 
-final class AuthDTO extends Data
+final class LoginDTO extends Data
 {
     public function __construct(
         public readonly string $email,
         public readonly string $password,
         public readonly bool $remember = false,
-    )
-    {
-
-    }
+    ) {}
 }
