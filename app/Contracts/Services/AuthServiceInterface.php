8cc47f5a9787ea0dd34c63149f9ba1c32b918c21
George Repiev <egor.repiev@gmail.com>
2025-11-23 23:15:44 +0500

    refactor(user): обновить интерфейс класса-сервиса аутентификации

--- app/Contracts/Services/AuthServiceInterface.php
+++ app/Contracts/Services/AuthServiceInterface.php
@@ -2,9 +2,10 @@
 
 namespace App\Contracts\Services;
 
+use App\Exceptions\User\UserLoginException;
 use App\Exceptions\User\UserRegistrationException;
 use App\Models\User;
-use App\Structures\Users\RegistrationDTO;
+use App\Structures\Users\{LoginDTO, RegistrationDTO};
 
 interface AuthServiceInterface
 {
@@ -17,4 +18,29 @@ interface AuthServiceInterface
      * @throws UserRegistrationException Если регистрация не удалась - выбрасывается Exception
      */
     public function register(RegistrationDTO $user): User;
+
+    /**
+     * Функция аутентификации пользователя в системе.
+     *
+     * @param LoginDTO $user DTO-структура аутентификации пользователя
+     * @return array{0: User, 1: string} Массив с Eloquent-моделью пользователя и токеном
+     *
+     * @throws UserLoginException Если авторизация не удалась
+     */
+    public function login(LoginDTO $user): array;
+
+    /**
+     * Функция выхода пользователя из системы.
+     *
+     * @return void
+     */
+    public function logout(): void;
+
+    /**
+     * Функция обновления токена пользователя.
+     *
+     * @param  User $user Eloquent-модель пользователя
+     * @return string Новый токен пользователя
+     */
+    public function refreshToken(User $user): string;
 }
