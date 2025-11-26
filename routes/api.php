d36cb008b74295174f1856d0dc9cb3b02c2c33aa
George Repiev <repev.egor@nttek.ru>
2025-11-26 16:29:28 +0500

    feat(route): зарегистрировать маршруты и прибиндить их к контроллеру

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Post\PostController;
 use App\Http\Controllers\User\AuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +16,28 @@ Route::prefix('user')->group(function() {
         Route::delete('/logout', [AuthController::class, 'logout'])
             ->name('logout');
     });
+});
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::prefix('posts')->group(function() {
+        Route::post('/', [PostController::class, 'create'])
+            ->name('posts.create');
+    });
+
+    Route::prefix('post')->group(function() {
+        Route::put('/{id}', [PostController::class, 'update'])
+            ->name('posts.update');
+        Route::delete('/{id}', [PostController::class, 'delete'])
+            ->name('posts.delete');
+    });
+});
+
+Route::prefix('posts')->group(function() {
+    Route::get('/', [PostController::class, 'index'])
+        ->name('posts.index');
+});
+
+Route::prefix('post')->group(function() {
+    Route::get('/{id}', [PostController::class, 'read'])
+        ->name('posts.read');
 });
