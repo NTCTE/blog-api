31dda4c893ff917f6d73a4df25775f1df8ca405c
George Repiev <repev.egor@nttek.ru>
2025-11-26 16:29:01 +0500

    feat(post): создать контроллер

---
+++ app/Http/Controllers/Post/PostController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Contracts\Services\PostServiceInterface;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\CreatePostRequest;
+use App\Http\Requests\Post\UpdatePostRequest;
+use App\Http\Resources\Post\PostResource;
+use App\Structures\Post\EditPostDTO;
+use App\Structures\Post\PostDTO;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+    public function __construct(
+        private PostServiceInterface $service
+    ) {}
+
+    public function index(Request $request)
+    {
+
+    }
+
+    public function create(CreatePostRequest $request): JsonResource
+    {
+        $data = PostDTO::from($request->validated());
+
+        $post = $this->service->create(
+            Auth::id(),
+            $data,
+        );
+
+        return $post->toResource(PostResource::class);
+    }
+
+    public function read(Request $request, int $id): JsonResource
+    {
+        $post = $this->service->read(
+            $id,
+            Auth::guard('sanctum')->id(),
+        );
+
+        return $post->toResource(PostResource::class);
+    }
+
+    public function update(UpdatePostRequest $request, int $id): JsonResource
+    {
+        $data = EditPostDTO::from($request->validated());
+
+        $post = $this->service->update(
+            Auth::id(),
+            $id,
+            $data,
+        );
+
+        return $post->toResource(PostResource::class);
+    }
+
+    public function delete(Request $request, int $id): JsonResponse
+    {
+        $this->service->delete(
+            Auth::id(),
+            $id,
+        );
+
+        return response()->json([], 204);
+    }
+}
