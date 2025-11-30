1820141e0d78a15dfdc4876ab221a4f0df75052c
George Repiev <egor.repiev@gmail.com>
2025-11-30 16:55:56 +0500

    feat(lang): добавить локализацию для исключений комментариев

---
+++ lang/en/comment.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'exception' => [
+        'create' => 'Failed to create comment.',
+        'update' => 'Failed to update comment.',
+        'not_found' => 'Comment not found.',
+        'denied' => 'Access to comment denied.',
+    ],
+];
