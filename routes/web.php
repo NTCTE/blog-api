997299d44f8ef217d8a2195bf5f0e0c9d11792f5
George Repiev <egor.repiev@gmail.com>
2025-12-01 23:29:16 +0500

    feat(auth): добавить маршруты для аутентификации SPA

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,12 @@
 <?php
 
+use App\Http\Controllers\User\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('spa')->group(function() {
+    Route::post('/login', [AuthController::class, 'spaLogin'])
+        ->name('spa.login');
+    Route::delete('/logout', [AuthController::class, 'spaLogout'])
+        ->name('spa.logout')
+        ->middleware('auth:sanctum');
+});
