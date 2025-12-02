eb09f7febac3f1d54bc50bf81118abe1c6e97a0c
George Repiev <repev.egor@nttek.ru>
2025-12-02 16:25:47 +0500

    refactor(routes): производить рефактор кодовой базы маршрутизатора API

--- routes/api.php
+++ routes/api.php
@@ -1,43 +1,55 @@
 <?php
 
+use App\Http\Controllers\Comment\CommentController;
 use App\Http\Controllers\Post\PostController;
 use App\Http\Controllers\User\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('user')->group(function() {
-    Route::middleware('guest')->group(function() {
-        Route::post('/register', [AuthController::class, 'register'])
-            ->name('register');
-        Route::post('/login', [AuthController::class, 'login'])
-            ->name('login');
-    });
-
-    Route::middleware('auth:sanctum')->group(function() {
-        Route::delete('/logout', [AuthController::class, 'logout'])
-            ->name('logout');
-    });
+Route::prefix('posts')->group(function() {
+    Route::get('/', [PostController::class, 'index'])
+        ->name('posts.index');
+    Route::get('/{id}', [PostController::class, 'read'])
+        ->name('posts.read');
+    Route::get('/{post_id}/comments', [CommentController::class, 'byPost'])
+        ->name('posts.comments');
 });
 
+Route::prefix('comment/{comment_id}')->group(function() {
+    Route::get('/', [CommentController::class, 'read'])
+        ->name('comment.read');
+    Route::get('/replies', [CommentController::class, 'replies'])
+        ->name('comment.replies');
+});
+
+Route::middleware('guest')->prefix('user')
+    ->group(function() {
+        Route::post('/register', [AuthController::class, 'register'])
+            ->name('auth.register');
+        Route::post('/login', [AuthController::class, 'login'])
+            ->name('auth.login');
+    });
+
 Route::middleware('auth:sanctum')->group(function() {
+    Route::prefix('user')->group(function() {
+        Route::delete('/logout', [AuthController::class, 'logout'])
+            ->name('auth.logout');
+    });
+
     Route::prefix('posts')->group(function() {
         Route::post('/', [PostController::class, 'create'])
             ->name('posts.create');
-    });
-
-    Route::prefix('post')->group(function() {
         Route::put('/{id}', [PostController::class, 'update'])
             ->name('posts.update');
         Route::delete('/{id}', [PostController::class, 'delete'])
             ->name('posts.delete');
     });
-});
 
-Route::prefix('posts')->group(function() {
-    Route::get('/', [PostController::class, 'index'])
-        ->name('posts.index');
-});
-
-Route::prefix('post')->group(function() {
-    Route::get('/{id}', [PostController::class, 'read'])
-        ->name('posts.read');
+    Route::prefix('comment')->group(function() {
+        Route::post('/', [CommentController::class, 'create'])
+            ->name('comment.create');
+        Route::put('/{comment_id}', [CommentController::class, 'update'])
+            ->name('comment.update');
+        Route::delete('/{comment_id}', [CommentController::class, 'delete'])
+            ->name('comment.delete');
+    });
 });
