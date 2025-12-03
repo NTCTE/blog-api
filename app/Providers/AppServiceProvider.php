8431c8c8e59ef23b6e189a6c4155c0637cd2cea9
George Repiev <repev.egor@nttek.ru>
2025-12-03 14:21:28 +0500

    feat(core): произвести биндинг интерфейса с сервисом

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Contracts\Services\AuthServiceInterface;
 use App\Contracts\Services\CommentServiceInterface;
+use App\Contracts\Services\LikeInterface;
 use App\Contracts\Services\PostServiceInterface;
 use App\Services\Comment\CommentService;
+use App\Services\Like\LikeService;
 use App\Services\Post\PostService;
 use App\Services\User\AuthService;
 use Illuminate\Support\ServiceProvider;
@@ -20,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(AuthServiceInterface::class, AuthService::class);
         $this->app->bind(PostServiceInterface::class, PostService::class);
         $this->app->bind(CommentServiceInterface::class, CommentService::class);
+        $this->app->bind(LikeInterface::class, LikeService::class);
     }
 
     /**
