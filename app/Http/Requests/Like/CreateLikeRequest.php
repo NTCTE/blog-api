c0e346fd1b30fe5c18ced9b25e540aa7677ea8cc
George Repiev <repev.egor@nttek.ru>
2025-12-03 14:23:10 +0500

    feat(likes): создать запрос на создание лайка

---
+++ app/Http/Requests/Like/CreateLikeRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests\Like;
+
+use App\Enums\Likes\MorphModelsEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CreateLikeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'model' => [
+                'required',
+                Rule::enum(MorphModelsEnum::class),
+            ],
+            'model_id' => [
+                'required',
+                'int',
+            ],
+            'is_like' => [
+                'required',
+                'boolean',
+            ],
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'model' => __('attributes.like.model'),
+            'model_id' => __('attributes.like.model_id'),
+            'is_like' => __('attributes.like.is_like'),
+        ];
+    }
+}
